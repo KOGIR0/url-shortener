40fc88f603bb587d9e92fa9886dacb15428ea358
IGOR <kondrahov.2000@mail.ru>
2021-10-02 14:34:14 +0300

    Added redirect requests

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # URL shortener app
+-----
+
+
+## Start
+
+npm start
+
+-----
+## Usage
+
+### Create redirect
+
+Redirects from /surl to www.google.com
+#### custom url
+REQEST
+curl -X POST http://localhost:3000/www.google.com?shortUrl=surl
+RESPONSE
+surl
+
+#### random url
+REQUEST
+curl -X POST http://localhost:3000/www.google.com
+RESPONSE
+generated url
--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 import express from 'express';
 
+let shortUrls = {};
+
 let app = express();
 
 app.get('/', (req, res) => {
     res.send("Hello, world");
+});
+
+app.get('/:shorturl', (req, res) => {
+    if(shortUrls[req.params.shorturl]) {
+        res.redirect("https://" + shortUrls[req.params.shorturl]);
+    } else {
+        res.status(404).send("Unknown url");
+    }
+});
+
+app.post('/:url', (req, res) => {
+    const shortUrl = Date.now().toString(36);
+    if(req.query.shortUrl) {
+        shortUrls[req.query.shortUrl] = req.params.url;
+        res.status(200).send(req.protocol + '://' + req.get('host') + '/' + req.query.shortUrl + '\n');
+    } else {
+        shortUrls[shortUrl] = req.query.url;
+        res.status(200).send(req.protocol + '://' + req.get('host') + '/' + shortUrl + '\n');
+    }
 });
 
 app.listen(3000, () => {
